4ae90b294585456aa3d79087cc002a1f1789adba
Saffron <saffronbeatson@gmail.com>
2016-04-05 13:03:37 +0200

    worked slowly but heres what i got

--- balloon.js
+++ balloon.js
@@ -1,6 +1,3 @@
-
-// var furtherClean = "[ [ '4', ' red' ], [ '3', ' blue' ], [ 'and 3', ' yellow' ] ]"
-
 exports.smallerSentence = function(string) {
 
   var cleanUp = string.replace(" and", "").replace(".", "").split(", ")
@@ -9,21 +6,33 @@ exports.smallerSentence = function(string) {
   cleanUp.forEach(function(littleStrings) {
     infoArray.push(littleStrings.split(" sets of "));
   });
-  // console.log (furtherClean);
   return infoArray;
 };
 
 
 exports.balloonMap = function(infoArray) {
-var mappedArray = [];
+  var mappedArray = [];
 
-infoArray.forEach(function (infoArray){
-  var balloonObject = {
-  colour: infoArray[1],
-  number: Number(infoArray[0])
-}
+  infoArray.forEach(function(infoArray) {
+    var balloonObject = {
+      colour: infoArray[1],
+      number: Number(infoArray[0])
+    }
+console.log (balloonObject);
+    mappedArray.push(balloonObject);
+  })
+  return mappedArray;
+};
 
-mappedArray.push(balloonObject);
-})
-return mappedArray;
+exports.balloonCalculator = function(sets, mappedArray) {
+  var totalNumber = [];
+  mappedArray.forEach(function (balloonInfo){
+  totalNumber.push(balloonInfo.number*sets);
+
+});
+//battled to figure out how to map back to the mappedArray after using a forEach.
+
+console.log (totalNumber);
+return totalNumber;
+
 };
--- test/balloon_test.js
+++ test/balloon_test.js
@@ -2,6 +2,7 @@ var assert = require("assert");
 var balloon = require("../balloon.js");
 
 describe("For the Balloon code kata I should", function() {
+
   it("create a function that returns a string with just the colour and number of balloons", function() {
     var result = balloon.smallerSentence("4 sets of red, 3 sets of blue, and 3 sets of yellow.");
     assert.deepEqual(result, [
@@ -11,7 +12,7 @@ describe("For the Balloon code kata I should", function() {
     ]);
 
   });
-
+//
   it("create a function that returns an object matching the colour and number of balloons", function() {
     var result = balloon.balloonMap([
       ['4', 'red'],
@@ -21,15 +22,13 @@ describe("For the Balloon code kata I should", function() {
     assert.deepEqual(result,[{ colour: 'red', number: 4 },
   { colour: 'blue', number: 3 },
   { colour: 'yellow', number: 3 }]);
-
-
-  it("create a function that multiplies each set of balloons by 3 and returns the total number of balloons ordered by each colour", function() {
-    var result = balloon.balloonCalculator(3)
-    assert.deepEqual(result, [{ colour: 'red', number: 12 },
-  { colour: 'blue', number: 9 },
-  { colour: 'yellow', number: 9 }])
-
-
 });
+//
+  it("create a function that multiplies each set of balloons by 3 and returns the total number of balloons ordered by each colour", function() {
+    var result = balloon.balloonCalculator(3, [{ colour: 'red', number: 4 },
+  { colour: 'blue', number: 3 },
+  { colour: 'yellow', number: 3 }])
+    assert.deepEqual(result, [ 12, 9, 9 ]);
+
 });
 });
